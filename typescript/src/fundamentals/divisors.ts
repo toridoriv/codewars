8527b619c0147c6156960a7e76f769f5beb6661c
toridoriv <vrodriguezfe@icloud.com>
2021-06-24 10:37:06 -0400

    perf(fundamentals): Improve divisors performance

--- typescript/src/fundamentals/divisors.ts
+++ typescript/src/fundamentals/divisors.ts
@@ -2,9 +2,7 @@ export const divisors = (
   x: number,
   counter: number = 0,
   current: number = x
-): number => {
-  const total = x % current === 0 ? counter + 1 : counter;
-  return current === 1 ? total : divisors(x, total, current - 1);
-};
-
-divisors(4);
+): number =>
+  current === 0
+    ? counter
+    : divisors(x, x % current === 0 ? counter + 1 : counter, current - 1);
